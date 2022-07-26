d81119e99c95ce7e7fd54f83fdb659f65fbcdbb1
fab <fablol@users.noreply.github.com>
2022-07-27 07:56:09 +0800

    Update DockerBuilder.sh

--- DockerBuilder.sh
+++ DockerBuilder.sh
@@ -4,6 +4,6 @@ echo Building fablol/lede
 docker build -t fablol/build-lede-builder -m 2GB -f .\Dockerfile.builder .
 
 docker create --name extract fablol/build-lede-builder
-mkdir ./bin
-docker cp extract:/lede/bin/targets/x86/64/* ./bin
+mkdir -p ./bin
+docker cp extract:/lede/bin/targets/x86/64/openwrt-x86-64-generic-ext4-combined-efi.img ./bin
 docker rm -f extract
